18862b8158822e5c422c0e42c7867fc0379f29ef
William Hulme <wjchulme@outlook.com>
2019-01-29 16:00:17 +0000

    Updating dordinal and pordinal to forbid character inputs

--- R/ordinal_distribution.R
+++ R/ordinal_distribution.R
@@ -4,7 +4,7 @@
 #' @param p vector of probabilities
 #' @param n number of random values to return
 #' @param prob vector of probabilities for observing each value
-#' @param as.numeric logicial; return value as numeric or character?
+#' @param as.numeric logical; return value as numeric or character?
 #' @param log logical; if TRUE, probabilities p are given as log(p)
 #' @name ordinal
 #' @examples
@@ -12,33 +12,21 @@
 NULL
 
 
-# random value from ordinal distribution
-#' @rdname ordinal
-#' @export
-rordinal <- function(n, prob, as.numeric = FALSE){
-
-  prob <- prob/sum(prob)  #normalise
-  counts <- apply(rmultinom(n, 1, prob = prob)==1, 1, sum)
-
-  if(as.numeric) sample(rep(seq_along(counts), counts))
-  else if (!is.null(names(prob))) sample(rep(names(counts), counts))
-  else sample(rep(as.character(seq_along(counts)), counts))
-}
-
 
 # probability mass function (or density)
 #' @rdname ordinal
 #' @export
 dordinal <- function(x, prob, log = FALSE){
 
-  if(is.numeric(x))   if(!all(x %in% seq_along(prob)))  stop("numeric index out-of-bounds")
-  if(is.character(x)) if(!all(x %in% names(prob))) stop("character index out-of-bounds")
+  if (!is.numeric(x) | !all(x>=0)) stop("x must be non-negative numeric")
+  if (!all(x%%1==0)) warning("non-integer x: density given as zero")
 
-  prob <- prob/sum(prob)  #normalise
+  prob <- c(0, prob/sum(prob), 0)  #normalise
 
-  if(log) log(prob[x])
-  else prob[x]
+  if (log) log( prob[pmin(pmax(x+1,1),length(prob))]*((x%%1)==0)*1 )
+  else prob[pmin(pmax(x+1,1),length(prob))]*((x%%1)==0)*1
 }
+
 
 
 # distribution function
@@ -46,21 +34,22 @@ dordinal <- function(x, prob, log = FALSE){
 #' @export
 pordinal <- function(q, prob, log = FALSE){
 
-  if(is.numeric(q))   if(!all(q %in% seq_along(prob)))  stop("numeric index out-of-bounds")
-  if(is.character(q)) if(!all(q %in% names(prob))) stop("character index out-of-bounds")
+  if (!is.numeric(q) | !all(q>=0)) stop("q must be non-negative numeric")
 
-  prob <- prob/sum(prob) #normalise
-  cumulprob <- cumsum(prob)
+  q <- floor(q)
+  prob <- prob/sum(prob)
+  cumulprob <- c(0,cumsum(prob))
 
-  if(log) log(cumulprob[q])
-  else cumulprob[q]
+  if (log) log(cumulprob[pmin(pmax(q+1,1),length(cumulprob))])
+  else cumulprob[pmin(pmax(q+1,1),length(cumulprob))]
 }
+
 
 
 # quantile function
 #' @rdname ordinal
 #' @export
-qordinal <- function(p, prob, as.numeric = FALSE){
+qordinal <- function(p, prob, as.numeric = TRUE){
 
   if (!all(p>=0 & p<=1)) stop("p must be between 0 and 1")
 
@@ -71,3 +60,20 @@ qordinal <- function(p, prob, as.numeric = FALSE){
   else if (!is.null(names(prob))) unlist(lapply(p, function(p, cumulprob){ names(cumulprob)[min(which(p<=cumulprob))]}, cumulprob=cumulprob))
   else unlist(lapply(p, function(p, cumulprob){ as.character(seq_along(cumulprob))[min(which(p<=cumulprob))]}, cumulprob=cumulprob))
 }
+
+
+
+# random value from ordinal distribution
+#' @rdname ordinal
+#' @export
+rordinal <- function(n, prob, as.numeric = TRUE){
+
+  prob <- prob/sum(prob)  #normalise
+  counts <- apply(rmultinom(n, 1, prob = prob)==1, 1, sum)
+
+  if(as.numeric) sample(rep(seq_along(counts), counts))
+  else if (!is.null(names(prob))) sample(rep(names(counts), counts))
+  else sample(rep(as.character(seq_along(counts)), counts))
+}
+
+
